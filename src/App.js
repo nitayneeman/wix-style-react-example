b27f28e07ede5a2936aedf397ebde88e552d9301
nitayneeman <nitayneeman@users.noreply.github.com>
2019-06-03 14:15:48 +0300

    Using <TextButton/>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TextButton from 'wix-style-react/TextButton';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,17 +9,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TextButton as="a" href="https://wix-wix-style-react.surge.sh/" target="_blank">
+          Check out the docs
+        </TextButton>
       </header>
     </div>
   );
